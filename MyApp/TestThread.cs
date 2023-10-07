146f4f1884dbba6728dc7bc103187019549c46ba
“inakuuun” <bkg.inamoto@gmail.com>
2023-10-07 17:04:46 +0900

    ソース整理 - ログファイル名取得方法修正

--- MyApp/TestThread.cs
+++ MyApp/TestThread.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyApp.Db;
 using MyApp.Logs;
 using MyApp.Threads;
 
@@ -15,8 +16,15 @@ namespace MyApp
         /// </summary>
         private string _logFileName { get => base.ThreadName ?? string.Empty; }
 
+        /// <summary>
+        /// DBロジック
+        /// </summary>
+        private DbLogic? _dbLogic;
+
         protected override bool RunInit()
         {
+            _dbLogic = new DbLogic();
+            _dbLogic.ChatDaoAccess.ChatInsert();
             Log.Trace(_logFileName, "呼び出し元が合っているかテスト");
             return true;
         }
